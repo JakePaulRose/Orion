238a7e2553ffb57f485fbc5c766f3d8c25443626
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-07 20:46:49 +0000

    Added tools into orionPy

--- OrionPY.py
+++ OrionPY.py
@@ -49,3 +49,41 @@ class OrionTools:
 
         self.calc_mag_mean = np.nanmean(self.mag_rebin)
         self.calc_mag_rms = np.sqrt((np.nanmean(np.square(self.mag_rebin))/np.size(self.mag_rebin)))
+
+    
+class Tools:
+    
+    def __init__(self):
+        self.hdu=self.load_file('/data/jpr64/NG0535-0523_802_2017,2017S_CYCLE1807.fits')
+
+    @staticmethod
+    def load_file(filename):
+            return fits.open(filename)
+    
+    def load_data_series(self, calibrate_time = True):
+        """Loads the time series and flux for a given obj_index (not id) then removes points with 0 flux. """
+        self.time = self.hdu['hjd'].data
+        self.time = self.time / (24*60*60)
+        self.flux = self.hdu['sysrem_flux3'].data
+        self.flux[self.flux == 0] = np.nan
+        self.mag = -2.5*np.log10(self.flux)
+
+    def load_means(self):
+        """Loads the mean flux, for every object in catalogue"""
+        self.flux_mean = self.hdu['catalogue'].data['flux_mean']
+        self.flux_rms = self.hdu['catalogue'].data['flux_wrms']
+        self.mag_mean = self.hdu['catalogue'].data['NGTS_gmag']
+        self.mag_rms = self.hdu['catalogue'].data['mag_rms']
+        self.gaia_gmag = self.hdu['catalogue'].data['gaia_gmag']
+        self.gaia_gmag_err = self.hdu['catalogue'].data['gaia_gmag_err']
+    
+    def rebin(self):
+        """Returns rebinned time and flux set to 6 min bins."""
+
+        self.time_rebin, self.mag_rebin, junk = bin_tools.rebin_err_chunks(self.time, self.mag, dt=(1/240), max_gap=0.5)
+        
+    def rebinned_vals(self):
+        """Calculates the new rebinned mean and rms"""
+
+        self.calc_mag_mean = np.nanmean(self.mag_rebin)
+        self.calc_mag_rms = np.sqrt((np.nanmean(np.square(self.mag_rebin))/np.size(self.mag_rebin)))     
