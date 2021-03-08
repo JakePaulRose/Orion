f151bfd6d42c8cb1b420013d1b141b8fb80a929a
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-08 18:59:38 +0000

    removed break to allow full run

--- rebin_data.py
+++ rebin_data.py
@@ -11,13 +11,13 @@ flux_time_stack = np.stack((data.time, data.flux), axis = 2)
 
 rebinned_data = []
 
-j = 0
+# j = 0
 
 for i in flux_time_stack:
      rebinned_flux, rebinned_time, junk = bin_tools.rebin_err_chunks(i[:,0], i[:,1], dt=(1/240), max_gap=0.5)
      rebinned_data.append(list(zip(rebinned_time,rebinned_flux)))
-     j += 1
-     if j == 5: break
+    #  j += 1
+    #  if j == 5: break
 
 
 """Saving the file"""
