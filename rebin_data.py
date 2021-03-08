9679b10e04c5d3762a35832f78fb79f1f57103ac
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-08 18:54:36 +0000

    changed number of lightcurves to 5 for test

--- rebin_data.py
+++ rebin_data.py
@@ -17,7 +17,7 @@ for i in flux_time_stack:
      rebinned_flux, rebinned_time, junk = bin_tools.rebin_err_chunks(i[:,0], i[:,1], dt=(1/240), max_gap=0.5)
      rebinned_data.append(list(zip(rebinned_time,rebinned_flux)))
      j += 1
-     if j == 3: break
+     if j == 5: break
 
 
 """Saving the file"""
