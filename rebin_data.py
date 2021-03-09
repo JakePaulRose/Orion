3f6b1f9b01dd8c633dfa3689ed1c59c308da8a9d
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-09 21:00:33 +0000

    removed breaks and changed save name

--- rebin_data.py
+++ rebin_data.py
@@ -12,19 +12,20 @@ flux_time_stack = np.stack((data.time, data.flux), axis = 2)
 
 rebinned_data = []
 
-j = 0
+# j = 0
 start = timeit()
 for i in flux_time_stack:
      rebinned_flux, rebinned_time, junk = bin_tools.rebin_err_chunks(i[:,0], i[:,1], dt=(1/240), max_gap=0.5)
      rebinned_data.append(np.stack((rebinned_time, rebinned_flux), axis = 1))
      
-     j += 1
-     if j == 10: break
-end = timeit()
+    #  j += 1
+    #  if j == 10: break
+# end = timeit()
 
-print(start-end)
+# print(start-end)
 x = np.asarray(rebinned_data, dtype = object)
 
 """Saving the file"""
 
-np.save('test.npy',x)
+np.save('rebinned_data.npy',x)
+
