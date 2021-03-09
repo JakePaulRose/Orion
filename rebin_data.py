650b055497d2c4534a998191960132a0583b8de7
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-09 16:43:34 +0000

    Changed it so it should export a numpy array instead

--- rebin_data.py
+++ rebin_data.py
@@ -11,15 +11,15 @@ flux_time_stack = np.stack((data.time, data.flux), axis = 2)
 
 rebinned_data = []
 
-# j = 0
+# j = 0/
 
 for i in flux_time_stack:
      rebinned_flux, rebinned_time, junk = bin_tools.rebin_err_chunks(i[:,0], i[:,1], dt=(1/240), max_gap=0.5)
-     rebinned_data.append(list(zip(rebinned_time,rebinned_flux)))
-    #  j += 1
-    #  if j == 5: break
+     rebinned_data.append(np.array(list(zip(rebinned_time,rebinned_flux))))
+     j += 1
+     if j == 5: break
 
 
 """Saving the file"""
-pd.DataFrame(rebinned_data).to_pickle("rebinned_data.pkl")
+pd.DataFrame(rebinned_data).to_pickle("rebinned_test.pkl")
 
