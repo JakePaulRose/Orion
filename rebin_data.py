6b73adfd6f13951f692ecf4d3ba1d069cf7369dd
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-09 20:39:38 +0000

    added timing to see how fast it is

--- rebin_data.py
+++ rebin_data.py
@@ -4,6 +4,7 @@ from astropy.io import fits
 import bin_tools
 from OrionPY import OrionTools, Tools
 import pandas as pd
+from timeit imp
 
 data = Tools()
 
@@ -12,14 +13,16 @@ flux_time_stack = np.stack((data.time, data.flux), axis = 2)
 rebinned_data = []
 
 j = 0
-
+start = timeit()
 for i in flux_time_stack:
      rebinned_flux, rebinned_time, junk = bin_tools.rebin_err_chunks(i[:,0], i[:,1], dt=(1/240), max_gap=0.5)
      rebinned_data.append(np.stack((rebinned_time, rebinned_flux), axis = 1))
      
      j += 1
      if j == 5: break
+end = timeit()
 
+print(end-start)
 x = np.asarray(rebinned_data, dtype = object)
 
 """Saving the file"""
