49f9a7235ad417604214a59db2c548f2cd0c6469
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-09 16:45:05 +0000

    added j =0

--- rebin_data.py
+++ rebin_data.py
@@ -11,7 +11,7 @@ flux_time_stack = np.stack((data.time, data.flux), axis = 2)
 
 rebinned_data = []
 
-# j = 0/
+j = 0
 
 for i in flux_time_stack:
      rebinned_flux, rebinned_time, junk = bin_tools.rebin_err_chunks(i[:,0], i[:,1], dt=(1/240), max_gap=0.5)
