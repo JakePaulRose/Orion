76a5da1f8983298929a27a946c60a69c9f951ccf
Jakerstudent <41909280+Jakerstudent@users.noreply.github.com>
2021-03-10 14:45:03 +0000

    added break for test

--- rebin_data.py
+++ rebin_data.py
@@ -13,14 +13,14 @@ flux_time_stack = np.stack((data.time, data.flux), axis = 2)
 rebinned_data = []
 
 
-# j = 0
+j = 0
 start = timeit()
 for i in flux_time_stack:
      rebinned_flux, rebinned_time, rebinned_flux_err = bin_tools.rebin_err_chunks(i[:,0], i[:,1], dt=(1/240), max_gap=0.5)
      rebinned_data.append(np.stack((rebinned_time, rebinned_flux_err, rebinned_flux_err), axis = 1))
 
-    #  j += 1
-    #  if j == 10: break
+     j += 1
+     if j == 10: break
 end = timeit()
 
 print(start-end)
